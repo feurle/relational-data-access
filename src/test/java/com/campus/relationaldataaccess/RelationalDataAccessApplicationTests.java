46ac5ffb3cfdf1219692b49eeff257590ab3296c
Daniel feurle <feurle@gmail.com>
2021-06-25 14:59:49 +0200

    first commit

---
+++ src/test/java/com/campus/relationaldataaccess/RelationalDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.relationaldataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalDataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
